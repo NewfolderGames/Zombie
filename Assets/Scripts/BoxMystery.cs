d88e37b3991dd835e2d7e4d71ee8ee82ca08733b
이민용 <newfoldergames@gmail.com>
2017-02-13 21:38:46 +0900

    미스터리 상자 1
    상자 근처에 가서 E 키를 누르면 랜덤 무기 구매 가능
    현재 들고있는 무기와 교체됨

---
+++ Assets/Scripts/BoxMystery.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxMystery : MonoBehaviour {
+
+	public GameObject player;
+	public Player playerInfo;
+	public PlayerEquip playerWeapon;
+
+	public int boxCost;
+
+	void Start() {
+
+		player = GameObject.Find ("Player");
+		playerInfo = player.GetComponent<Player> ();
+		playerWeapon = GameObject.Find ("Player_Equip").GetComponent<PlayerEquip> ();
+
+	}
+
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.E)) {
+
+			if (Vector3.Distance (player.transform.position, transform.position) <= 5) {
+
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				RaycastHit rayHit;
+
+				float rayLenght = 500f;
+
+				int layerMask = LayerMask.GetMask ("BoxMystery");
+
+				if( Physics.Raycast( ray, out rayHit, rayLenght, layerMask ) ) {
+
+					if (playerInfo.playerPoint >= boxCost) {
+
+						playerWeapon.itemSlot [playerWeapon.itemSlotNumber] = playerWeapon.WeaponChange ((int)Random.Range (0f, 6f));
+						playerWeapon.WeaponSelect (playerWeapon.itemSlot [playerWeapon.itemSlotNumber]);
+						playerInfo.playerPoint -= boxCost;
+						playerInfo.TextUpdate ();
+
+					} else
+						Debug.Log ("돈이 부족합니다");
+
+				}
+					
+			}
+
+		}
+
+	}
+
+}
